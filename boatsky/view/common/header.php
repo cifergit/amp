18449cfa09b120d2e4d76eeb1c82f2fb0c9fb1a4
cifer <zeshare@qq.com>
2016-01-01 02:36:45 +0800

    submit

---
+++ boatsky/view/common/header.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @desc: 头部
+ * @author: cifer
+ * @date: 2016/01/01
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>头部</title>
+    <link rel="shortcut icon" href="../../favicon.ico">
+</head>
+
+<body>
+<div class="header">
+    头部
+ </div>
+</body>
+</html>
