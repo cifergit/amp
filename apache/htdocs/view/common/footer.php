09632d10c47e7ee0612249503b60ad7115676ac2
cifer <zeshare@qq.com>
2016-01-03 01:10:55 +0800

    add index page

---
+++ apache/htdocs/view/common/footer.php
@@ -0,0 +1,16 @@
+
+<?php
+/**
+ * desc: footer
+ * User: cifer 
+ * DateTime: 2016/1/1 18:22
+ */
+?>
+<footer>
+    <section id="userPv" class="user_pv"></section>
+    <section class="copyright">
+        <nav>
+            Copyright &copy; 2016-<?php echo date("Y");?> 太空船
+        </nav>
+    </section>
+</footer>
