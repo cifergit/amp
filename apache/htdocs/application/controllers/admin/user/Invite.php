35fb20d8814ebc1a6684f8011738b1cc93c76d20
cifer <zeshare@qq.com>
2016-06-10 16:32:15 +0800

    add invite code

---
+++ apache/htdocs/application/controllers/admin/user/Invite.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: cifer
+ * Date: 2016/6/10
+ * Time: 10:13
+ */ 
---
+++ apache/htdocs/application/models/admin/user/Model_invite.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * desc: 
+ * User: cifer
+ * Date: 2016/6/10
+ * Time: 11:09
+ */ 
