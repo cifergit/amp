c263fe95ae14bdefe254b8a9e11cc21e468d07c2
cifer <zeshare@qq.com>
2017-02-19 11:16:02 +0800

    add about and links

---
+++ apache/htdocs/application/views/common/author.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * desc: 
+ * User: cifer
+ * Date: 2017/2/9
+ * Time: 23:34
+ */?>
+<link rel="stylesheet" href="//www.boatsky.com/static/css/common/about.css"/>
+</head>
+<body>
+
+<?php $this->load->view('common/header');?>
+
+<section class="main-wrap js-main-wrap">
+    <section class="author-wrap mod-inner">
+        <P>cifer（跟着我读: 西法），一只前端攻城狮。</P>
+        <p>不懂经济的段子手不是好程序猿。</p>
+        <p>爱过————WOW小路痴，LOL老菜鸟。</p>
+        <details class="details">
+            <summary>曾经喜欢好电影</summary>
+            <ol type="1">
+                <li>《指环王三部曲》</li>
+                <li>《闻香识女人》</li>
+                <li>《黑客帝国三部曲》</li>
+                <li>《黄金三镖客》</li>
+                <li>《爱情三部曲》</li>
+                <li>《千年女优》</li>
+                <li>《缺席的人》</li>
+                <li>《成为简·奥斯汀》</li>
+                <li>《百万美元宝贝》</li>
+                <li>《夜访吸血鬼》</li>
+                <li>《秒速5厘米》</li>
+                <li>《七武士》</li>
+                <li>《降临》</li>
+                <li>《风之谷》</li>
+                <li>《本杰明·巴顿奇事》</li>
+                <li>《入殓师》</li>
+                <li>《星际穿越》</li>
+                <li>《搏击俱乐部》</li>
+                <li>《千与千寻》</li>
+                <li>《美丽心灵》</li>
+                <li>《潘神的迷宫》</li>
+                <li>《肖申克的救赎》</li>
+                <li>《大鱼》</li>
+                <li>《海洋之歌》</li>
+                <li>《无间道三部曲》</li>
+                <li>《阿修罗》</li>
+                <li>《金刚》</li>
+                <li>《加勒比海盗前三部》</li>
+                <li>《火星救援》</li>
+                <li>《云图》</li>
+                <li>《一代骄马》</li>
+                <li>《银翼杀手》</li>
+                <li>《范海辛》</li>
+                <li>《赛德克·巴莱》</li>
+                <li>《原罪》</li>
+                <li>《老无所依》</li>
+                <li>《为奴十二年》</li>
+                <li>《忠犬八公物语》</li>
+                <li>《蝙蝠侠前传三部曲》</li>
+                <li>《霸王别姬》</li>
+            </ol>
+        </details>
+        <details class="details">
+            <summary>现在喜欢看好书</summary>
+            <ol type="1">
+                <li>《国富论》</li>
+                <li>《浮士德》</li>
+                <li>《平凡的世界》</li>
+                <li>《飘》</li>
+                <li>《假如给我三天光明》</li>
+                <li>《围城》</li>
+                <li>《梦的解析》</li>
+                <li>《浪潮之巅》</li>
+                <li>《钢铁是怎样炼成的》</li>
+                <li>《中国近代史》</li>
+            </ol>
+        </details>
+        <p>生命不息，追求不止。</p>
+        <p>E-mail:  <a class="js-my-email" href="javascript:void(0)">点我</a></p>
+
+        <div class="friends">
+            <a href=""
+        </div>
+    </section>
+</section>
+<script type="text/javascript" data-main="/static/js/common/empty.js" src="/static/module/require/require.js"></script>
---
+++ apache/htdocs/application/views/common/friends.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * desc:
+ * User: cifer
+ * Date: 2017/2/9
+ * Time: 23:34
+ */?>
+<link rel="stylesheet" href="//www.boatsky.com/static/css/common/friends.css"/>
+</head>
+<body>
+
+<?php $this->load->view('common/header');?>
+
+<section class="main-wrap js-main-wrap">
+    <section class="friends-wrap mod-inner">
+        <h1>友链：</h1>
+
+        <ul class="friends-list">
+            <li>
+                <a target="_blank" href="http://www.byends.com" title="羽飞，风骚的前端程序猿">byends：</a>
+                <span>羽飞，风骚的前端哥</span>
+            </li>
+            <li>
+                <a target="_blank" href="http://www.yrczone.com/" title="coolcity,闷骚的C++程序猿">coolcity：</a>
+                <span>闷骚的C++攻城狮</span>
+            </li>
+        </ul>
+    </section>
+</section>
+<script type="text/javascript" data-main="/static/js/common/empty.js" src="/static/module/require/require.js"></script>
